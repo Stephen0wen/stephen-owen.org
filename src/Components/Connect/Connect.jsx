b00f7c734ef9575804d91d598f3acdaa58e3713a
Stephen0wen <stevejowen@gmail.com>
2024-05-12 23:38:32 +0100

    Adds mouseover to Connect page

--- src/Components/Connect/Connect.jsx
+++ src/Components/Connect/Connect.jsx
@@ -1,3 +1,4 @@
+import MouseOver from "../MouseOver/MouseOver";
 import "./Connect.css";
 
 const Connect = () => {
@@ -6,34 +7,40 @@ const Connect = () => {
             <div className="section-space connect">
                 <h2>Thanks for dropping by!</h2>
                 <div className="link-container">
-                    <a
-                        id="email-link"
-                        className="profile-link"
-                        aria-label="Send me an email"
-                        href="mailto:stevejowen@gmail.com"
-                    >
-                        <i className="fa-solid fa-envelope"></i>
-                        <span className="hidden">email</span>
-                    </a>
-                    <a
-                        id="linkedin-link"
-                        className="profile-link"
-                        aria-label="Visit my linkedin"
-                        target="_blank"
-                        href="https://www.linkedin.com/in/stephen-owen-00b7a52b8/"
-                    >
-                        <i className="fa-brands fa-linkedin"></i>
-                        <span className="hidden">linkedin</span>
-                    </a>
-                    <a
-                        id="github-link"
-                        className="profile-link"
-                        aria-label="Visit my github"
-                        target="_blank"
-                        href="https://github.com/Stephen0wen"
-                    >
-                        <i className="fa-brands fa-github"></i>
-                    </a>
+                    <MouseOver highlightClasses="extra-enlarge">
+                        <a
+                            id="email-link"
+                            className="profile-link"
+                            aria-label="Send me an email"
+                            href="mailto:stevejowen@gmail.com"
+                        >
+                            <i className="fa-solid fa-envelope"></i>
+                            <span className="hidden">email</span>
+                        </a>
+                    </MouseOver>
+                    <MouseOver highlightClasses="extra-enlarge">
+                        <a
+                            id="linkedin-link"
+                            className="profile-link"
+                            aria-label="Visit my linkedin"
+                            target="_blank"
+                            href="https://www.linkedin.com/in/stephen-owen-00b7a52b8/"
+                        >
+                            <i className="fa-brands fa-linkedin"></i>
+                            <span className="hidden">linkedin</span>
+                        </a>
+                    </MouseOver>
+                    <MouseOver highlightClasses="extra-enlarge">
+                        <a
+                            id="github-link"
+                            className="profile-link"
+                            aria-label="Visit my github"
+                            target="_blank"
+                            href="https://github.com/Stephen0wen"
+                        >
+                            <i className="fa-brands fa-github"></i>
+                        </a>
+                    </MouseOver>
                 </div>
                 <h3>Until next time...</h3>
             </div>
